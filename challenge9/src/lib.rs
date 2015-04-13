a9edb811362d9d75c250015b9bfd4ea028dd6f2b
Tim Taubert <tim@timtaubert.de>
2015-04-13 21:30:00 +0200

    correct pkcs7 padding

--- challenge9/src/lib.rs
+++ challenge9/src/lib.rs
@@ -8,11 +8,7 @@ impl PKCS7Pad for [u8] {
   fn pkcs7_pad(&self, len: usize) -> Vec<u8> {
     let mut data = Vec::from_iter(self.iter().map(|byte| *byte));
     let num = len - self.len() % len;
-
-    if num < len {
-      data.extend(Vec::from_iter(repeat(num as u8).take(num)));
-    }
-
+    data.extend(Vec::from_iter(repeat(num as u8).take(num)));
     data
   }
 }
@@ -23,8 +19,8 @@ mod test {
 
   #[test]
   fn test() {
-    assert_eq!(b"YELLOW SUBMARINE".pkcs7_pad(1), b"YELLOW SUBMARINE");
-    assert_eq!(b"YELLOW SUBMARINE".pkcs7_pad(16), b"YELLOW SUBMARINE");
+    assert_eq!(b"YELLOW SUBMARINE".pkcs7_pad(1), b"YELLOW SUBMARINE\x01");
+    assert_eq!(b"YELLOW SUBMARINE".pkcs7_pad(16), b"YELLOW SUBMARINE\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10");
     assert_eq!(b"YELLOW SUBMARINE".pkcs7_pad(17), b"YELLOW SUBMARINE\x01");
     assert_eq!(b"YELLOW SUBMARINE".pkcs7_pad(20), b"YELLOW SUBMARINE\x04\x04\x04\x04");
   }
